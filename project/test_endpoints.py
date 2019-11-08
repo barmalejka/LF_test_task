9e46a847c60075cd470ae289d5c03943e7f7c76f
barmalejka <33418735+barmalejka@users.noreply.github.com>
2019-11-08 17:38:27 +0200

    Add files via upload

---
+++ project/test_endpoints.py
@@ -0,0 +1,38 @@
+import pytest
+import requests
+from math import isclose
+
+url = 'http://127.0.0.1:5000'
+
+
+def test_index_page():
+    r = requests.get(url+'/')
+    assert r.status_code == 200
+
+
+def test_avg_elapsed_time():
+    r = requests.get(url+'/api/v1/queries/avg_elapsed_time?date=2019-10-18')
+    data = r.json()
+    assert r.status_code == 200
+    assert isclose(data['Average elapsed time, sec'], 35.60, rel_tol=1e-02)
+
+
+def test_avg_rows_per_sec():
+    r = requests.get(url+'/api/v1/queries/avg_rows_per_sec?start=2019-10-18+10:12:03&end=2019-10-18+10:28:03')
+    data = r.json()
+    assert r.status_code == 200
+    assert isclose(data['Average rows per sec'], 5467092.94, rel_tol=1e-02)
+
+
+def test_avg_rows_per_thread():
+    r = requests.get(url+'/api/v1/queries/avg_rows_per_thread?start=2018-10-18+10:12:03&end=2019-10-18+10:28:03')
+    data = r.json()
+    assert r.status_code == 200
+    assert isclose(data['Average rows per thread'], 24856407.93, rel_tol=1e-02)
+
+
+def test_avg_thread_per_sec():
+    r = requests.get(url+'/api/v1/queries/avg_thread_per_sec?start=2018-10-18+10:12:03&end=2019-10-18+10:28:03')
+    data = r.json()
+    assert r.status_code == 200
+    assert isclose(data['Average threads per sec'], 0.034, rel_tol=1e-02)
